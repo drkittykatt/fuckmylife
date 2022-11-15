a206562581eb2495da9fc266e75a8350ddd3ab29
drkittykatt <vixenaboutique@gmail.com>
2022-11-14 16:50:35 -0800

    added server

---
+++ packages/server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const helmet = require("helmet");
+const { Server } = require("socket.io");
+const app = express();
+
+const server = require("http").createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+app.use(helmet());
+app.use(express.json());
+
+// app.get("/api", (req, res) => {
+//   //res.json("hi")
+//   res.json({
+//     message: "Hello World",
+//   });
+// });
+
+io.on("connect", (socket) => {});
+
+server.listen(4000, () => {
+  console.log("server listening on port 4000");
+});
