bd5e4569588245eaf352cc0767ed8b930598695c
drkittykatt <vixenaboutique@gmail.com>
2022-11-14 18:02:18 -0800

    sharing validation

--- packages/app/components/Login/LoginScreen.jsx
+++ packages/app/components/Login/LoginScreen.jsx
@@ -2,25 +2,14 @@ import * as React from "react";
 import { Button, TextInput, View, Text, StyleSheet, Alert } from "react-native";
 import { Formik, ErrorMessage } from "formik";
 import { globalStyles } from "../../styles/global";
-import * as Yup from "yup";
-
-const SignupSchema = Yup.object().shape({
-  username: Yup.string()
-    .required("Username required!")
-    .min(6, "Username too short!")
-    .max(28, "Username too long!"),
-  password: Yup.string()
-    .required("Password required!")
-    .min(6, "Password too short!")
-    .max(28, "Password too long!"),
-});
+const { formSchema } = require("@whatsapp-clone/common");
 
 export default function LoginScreen({ navigation }) {
   return (
     <View style={globalStyles.container}>
       <Formik
         initialValues={{ username: "", password: "" }}
-        validationSchema={SignupSchema}
+        validationSchema={formSchema}
         onSubmit={(values, actions) => {
           const vals = { ...values };
           console.log(values);
--- packages/server/controllers/validateForm.js
+++ packages/server/controllers/validateForm.js
@@ -1,15 +1,4 @@
-const Yup = require("yup");
-
-const formSchema = Yup.object().shape({
-  username: Yup.string()
-    .required("Username required!")
-    .min(6, "Username too short!")
-    .max(28, "Username too long!"),
-  password: Yup.string()
-    .required("Password required!")
-    .min(6, "Password too short!")
-    .max(28, "Password too long!"),
-});
+const { formSchema } = require("@whatsapp-clone/common");
 
 const validateForm = (req, res) => {
   const formData = req.body;
