b39e99e74a1f4d11b27ea3d9e7a27bd7f07a76e1
drkittykatt <vixenaboutique@gmail.com>
2022-11-15 23:43:28 -0800

    added logout :D

--- packages/app/components/HomeScreen.jsx
+++ packages/app/components/HomeScreen.jsx
@@ -1,15 +1,22 @@
 import * as React from "react";
 import { Button, Text, View, SafeAreaView, StyleSheet } from "react-native";
+import * as SecureStore from "expo-secure-store";
 // import { AuthContext } from "./utils";
+import { AccountContext } from "./AccountContext";
 
-export default function HomeScreen() {
-  //   const { signOut } = React.useContext(AuthContext);
+export default function HomeScreen({ navigation }) {
+  const { setUser } = React.useContext(AccountContext);
+  // const { signOut } = React.useContext(AccountContext);
+  const signOut = () => {
+    SecureStore.deleteItemAsync("userToken");
+    setUser({ loggedIn: false });
+  };
   const { container } = styles;
 
   return (
     <SafeAreaView style={container}>
       <Text>Home screen!</Text>
-      {/* <Button title="Sign out" onPress={signOut} /> */}
+      <Button title="Sign out" onPress={signOut} />
     </SafeAreaView>
   );
 }
